ca3a4bbc0864ad5790794e86c2a86701fd41d518
bcy123185 <3134488921@qq.com>
2021-01-04 16:31:45 +0800

    CCCCC

---
+++ BLL/BCY/locationManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MODEL;
+using DAL.BCY;
+
+namespace BLL.BCY
+{
+    public class locationManager
+    {
+        public static IQueryable QueryLocation()
+        {
+            return locationService.QueryLocation();
+        }
+    }
+}
