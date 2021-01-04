13e76c526e9202d2039b487d3f494a4261f6cdee
keui12 <858762318@qq.com>
2021-01-04 16:10:08 +0800

    1111

---
+++ CangKu/Controllers/TLP/EntrepotController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CangKu.Controllers.TLP
+{
+    public class EntrepotController : Controller
+    {
+        // GET: Entrepot
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
